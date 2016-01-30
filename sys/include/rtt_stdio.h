8bf8a731f08f208041cd7035a5769a3efabcad06
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-01-29 22:44:39 -0800

    Add rtt_stdio supporting SEGGER RTT for stdin/stdout

---
+++ sys/include/rtt_stdio.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2016 Michael Andersen <m.andersen@berkeley.edu>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    sys_rtt_stdio SEGGER RTT stdio
+ * @ingroup     sys
+ *
+ * @brief       stdio init/read/write functions for SEGGER RTT. This is
+ *              designed to shadow the functions in uart_stdio
+ *
+ * @{
+ * @file
+ *
+ * @author      Michael Andersen <m.andersen@cs.berkeley.edu>
+ */
+#ifndef RTT_STDIO_H
+#define RTT_STDIO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief initialize the module. This is a noop.
+ */
+void uart_stdio_init(void);
+
+/**
+ * @brief read @p len bytes from stdio uart into @p buffer
+ *
+ * @param[out]  buffer  buffer to read into
+ * @param[in]   len     nr of bytes to read
+ *
+ * @return nr of bytes read
+ * @return <0 on error
+ */
+int uart_stdio_read(char* buffer, int len);
+
+/**
+ * @brief write @p len bytes from @p buffer into uart
+ *
+ * @param[in]   buffer  buffer to read from
+ * @param[in]   len     nr of bytes to write
+ *
+ * @return nr of bytes written
+ * @return <0 on error
+ */
+int uart_stdio_write(const char* buffer, int len);
+
+/**
+ * @brief enable stdin polling, at a power consumption cost
+ */
+void rtt_stdio_enable_stdin(void);
+
+#ifdef __cplusplus
+}
+#endif
+/** @} */
+#endif /* RTT_STDIO_H */
